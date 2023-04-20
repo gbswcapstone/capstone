16e26b92bad9fc1547dfa2de494e922e6509866e
chanbin <chanbin5634@gmail.com>
2023-04-20 16:57:46 +0900

    메인 수정

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 window.addEventListener('scroll', function(e) {
     console.log(this.window.scrollY);
+    if(this.window.scrollY == 721){
+      
+    }
     const bar = this.document.getElementById('bar');
     bar.style.width = (this.window.scrollY / 7)+"%";
   });
