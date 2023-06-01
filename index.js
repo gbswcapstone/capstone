a4edd43209c502f617677800c672ca7ed38482c1
chanbin <chanbin5634@gmail.com>
2023-06-01 16:48:02 +0900

    gle

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ window.addEventListener('scroll', function(e) {
         e.style.color = "#222";
       });
     }
+    else if(this.window.screenY > 1350){
+      
+    }
     else{
       document.querySelector('header').style.color = "white";
       document.querySelectorAll('header a').forEach((e)=>{
         e.style.color = "white";
       });
-      
     }
     const bar = this.document.getElementById('bar');
-    bar.style.width = (this.window.scrollY / 7)+"%";
+    bar.style.width = (this.window.scrollY / 14.3)+"%";
   });
