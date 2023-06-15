5d1d996ba6f10a6d7ac16cc623d764c2b6e7f0c4
cumedang <yusun2234sun@gmail.com>
2023-06-15 18:06:55 +0900

    지도api 추가및 메뉴 추가

--- trip/drog.js
+++
@@ -1,8 +0,0 @@
-const columns = document.querySelectorAll(".column");
-columns.forEach((column) => {
-  new Sortable(column, {
-    group: "shared",
-    animation: 150,
-    ghostClass: "blue-background-class"
-  });
-});
